4364328f2e13d7799c4705aac6c7258faf33e62e
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-15 15:40:16 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,19 +1,15 @@
 <?php
-//mendefinisikan konstanta
 $host = "localhost";
 $user = "ahmadabuhasan";
 $pass = "Salam 1 Jiwa";
 $dbnm = "ahmadabu_uas7";
 
-//membuat koneksi dengan database
-$conn = mysqli_connect($host, $user, $pass, $dbnm);
+//MySQLi Object-Oriented
+$conn = new mysqli($host, $user, $pass, $dbnm);
 
 // Check connection
-// if ($conn->connect_error) {
-//  die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
-// //mysqli_close($conn);
-// echo "<br>";
-// print_r(PDO::getAvailableDrivers());
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
