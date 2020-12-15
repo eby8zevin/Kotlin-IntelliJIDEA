a83c224531f53bb08a9de3ebf44d6e2a05d06ff4
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-15 15:12:49 +0700

    Create read.php

---
+++ API/read.php
@@ -0,0 +1,15 @@
+<?php
+include 'connection.php';
+
+$query = "SELECT * FROM data_mhs";
+$result = mysqli_query($koneksi, $query);
+$array_data = array();
+
+while($row = mysqli_fetch_assoc($result))
+{
+  $array_data[]=$row;
+}
+
+header('Content-type: text/javascript');
+echo json_encode($array_data, JSON_PRETTY_PRINT);
+?>
