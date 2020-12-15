292ac71731aec7e2945a84676f3257cb92790e7a
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-15 15:41:29 +0700

    Update connection.php

--- API/connection.php
+++ API/connection.php
@@ -4,5 +4,11 @@ $user = "ahmadabuhasan";
 $pass = "Salam 1 Jiwa";
 $dbnm = "ahmadabu_uas7";
 
-$conn = new mysqli($host, $user, $pass, $dbnm);
+$conn = mysqli_connect($host, $user, $pass, $dbnm);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
