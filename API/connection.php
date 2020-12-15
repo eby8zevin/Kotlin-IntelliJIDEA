90c2f8d7b96bcc0f83e1953eaee87a1b6bae04ba
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-15 15:33:22 +0700

    Update connection.php

--- API/connection.php
+++ API/connection.php
@@ -1,18 +1,8 @@
 <?php
-
 $host = "localhost";
 $user = "ahmadabuhasan";
 $pass = "Salam 1 Jiwa";
 $dbnm = "ahmadabu_uas7";
 
 $conn = new mysqli($host, $user, $pass, $dbnm);
-
-// Check connection
-if ($conn->connect_error) {
- die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-//mysqli_close($conn);
-echo "<br>";
-print_r(PDO::getAvailableDrivers());
 ?>
