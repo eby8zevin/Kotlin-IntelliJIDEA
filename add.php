b04e77d4427d3a0d95bf61f21071384fc2f68396
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 10:38:05 +0700

    Create add.php

---
+++ add.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Add</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+  <div align="center">
+    <form>
+      <label for="nim">NIM:</label><br>
+      <input type="number" id="nim" name="nim"><br>
+      <label for="nama">Nama:</label><br>
+      <input type="text" id="nama" name="nama">
+      <button type="submit" name="simpan">Simpan</button>
+    </form>
+  </div>
+
+</body>
+</html>
