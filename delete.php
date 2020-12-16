c396669da4c5c2f50d9e71b8577119c10a55246c
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 15:27:06 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+include "../connection.php";
+
+$id = $_GET['id'];
+
+$koneksi->query("DELETE FROM data_mhs WHERE data_id='$id'");
+
+echo "<script>alert('Data berhasil dihapus.');</script>";
+echo "<script>location='index.php?deletedata=succes';</script>";
+?>
