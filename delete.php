0a15dcbb94afa46c72fd25e4db523267f0ef42f8
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 16:19:18 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -5,6 +5,8 @@ $id = $_GET['id'];
 
 $conn->query("DELETE FROM data_mhs WHERE data_id='$id'");
 
-echo "<script>alert('Data berhasil dihapus.');</script>";
+echo "<script>alert('Data deleted successfully.');</script>";
 echo "<script>location='index.php?deletedata=succes';</script>";
+
+$conn->close();
 ?>
