40f190bd83610df053e4b9cb3c9b87526c6e5d81
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 13:54:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ include '../connection.php';
 <html lang="en">
 <head>
 <title>Web Service</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="WEB" />
+<meta name="author" content="Ahmad Abu Hasan" />
 </head>
 <body>
   <center><font color="red" size="100"><b>M A I N T E N A N C E</b></font></center>
@@ -29,7 +31,7 @@ include '../connection.php';
       <tbody>
         <?php $no = 1; ?>
         <?php $query = $conn->query("SELECT * FROM data_mhs ORDER BY data_nim ASC"); ?>
-        <?php while($row = $query->fetch_assoc()) { ?>
+        <?php while($row = $query->fetch_array()) { ?>
         <tr>
           <td><?php echo $no; ?></td>
           <td><?php echo $row['data_nim']; ?></td>
