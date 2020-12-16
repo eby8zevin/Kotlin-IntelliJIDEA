3028ba5022db9a0a27ac655d44427dcdf666ac88
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 11:02:10 +0700

    Update add.php

--- add.php
+++ add.php
@@ -6,15 +6,30 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-  <div align="center">
-    <form>
+  <h3>Form</h3>
+    <form action="" method="POST">
       <label for="nim">NIM:</label><br>
       <input type="number" id="nim" name="nim"><br>
       <label for="nama">Nama:</label><br>
       <input type="text" id="nama" name="nama">
       <button type="submit" name="simpan">Simpan</button>
     </form>
-  </div>
+
+<?php
+  if(isset($_POST['simpan'])) {
+  
+  $nim = $_POST['nim'];
+  $nama = $_POST['nama'];
+    
+  $query = $conn->query("SELECT * FROM data_mhs WHERE data_nim='$nim'");
+  if ($query->nuw_rows()==1) {
+    echo "<script>alert('NIM sudah digunakan.')</script>";
+		echo "<script>location='add.php'</script>";
+    }
+  }
+  
+  
+  ?>
 
 </body>
 </html>
