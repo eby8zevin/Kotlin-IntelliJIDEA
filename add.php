bb2f03e007bad1742d100dcfcfc0bf4ef5abbcc1
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 11:56:12 +0700

    Update add.php

--- add.php
+++ add.php
@@ -3,7 +3,9 @@
 <head>
 <title>Add</title>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="WEB" />
+<meta name="author" content="Ahmad Abu Hasan" />
 </head>
 <body>
   <h3>Form</h3>
