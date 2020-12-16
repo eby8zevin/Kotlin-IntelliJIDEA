8c4fef12a11281a042f9a6a87ecea632e2ceaaef
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-12-16 16:08:42 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -21,13 +21,28 @@ $row = $query->fetch_assoc();
   <h3>Form</h3>
     <form action="" method="POST">
       <label for="nim">NIM:</label><br>
-      <input type="number" id="nim" name="nim" value="<?php echo $row['data_nim']; ?>"><br>
+      <input type="number" id="nim" name="nim" value="<?php echo $row['data_nim']; ?>" readonly><br>
       <label for="nama">Nama:</label><br>
       <input type="text" id="nama" name="nama" value="<?php echo $row['data_nama']; ?>"><br><br>
       <button type="submit" name="update">Update</button>
     </form>
     
-
+<?php
+  if (isset($_POST['update'])) {
+  
+  $nim = $_POST['nim'];
+  $nama = $_POST['nama'];
+  
+  $conn->query("UPDATE data_mhs SET data_nim='$nim',
+                                    data_nama='$nama'
+                              WHERE data_id='$id'");
+  
+  echo "<script>alert('Data berhasil diupdate.');</script>";
+  echo "<script>location='index.php?editdata=succes';</script>";
+  }
+  
+$conn->close();
+?>
 
 </body>
 </html>
