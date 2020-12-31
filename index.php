71d534fba1a189f423a4ae15b7b7ab2c43886c42
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2020-12-31 18:33:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ include "../connection.php";
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>Web Service</title>
-<meta charset="UTF-8" />
+  <head>
+    <title>Web Service</title>
+    <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="description" content="WEB" />
-<meta name="author" content="Ahmad Abu Hasan" />
+<meta name="description" content="web" />
+<meta name="author" content="ahmadabuhasan" />
 <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
