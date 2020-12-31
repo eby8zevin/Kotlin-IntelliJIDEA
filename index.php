ae127b50016dafcad038cf70cbda9b3a0ea143f4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2020-12-31 18:35:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ include "../connection.php";
     <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
-<link rel="manifest" href="site.webmanifest">
-</head>
-<body>
+    <link rel="manifest" href="site.webmanifest">
+  </head>
+  <body>
   <center><font color="red" size="100"><b>php.id</b></font></center>
   <div align="center" padding="100px">
     <h3>My Project in PHP.ID<br>jancukers.com</h3>
