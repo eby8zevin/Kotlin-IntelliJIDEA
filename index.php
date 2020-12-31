589c61f2a5b11ad7186a8bfa84d347a32833e933
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2020-12-31 18:39:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -24,34 +24,34 @@ include "../connection.php";
         date_default_timezone_set('Asia/Jakarta');
         echo date('d F Y H:i:s'); 
         ?></p>
-    <a href="add.php">Add</a> || <a href="https://ahmadabuhasan.me/github/API">API</a> || <a href="https://github.com/eby8zevin/ahmadabuhasan.me">Source Code</a>
-    <table border="1" width="700px">
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>NIM</th>
-          <th>Name</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php $no = 1; ?>
-        <?php $query = $conn->query("SELECT * FROM data_mhs ORDER BY data_nim ASC"); ?>
-        <?php while($row = $query->fetch_assoc()) { ?>
-        <tr>
-          <td style="text-align: center;"><?php echo $no; ?></td>
-          <td><?php echo $row['data_nim']; ?></td>
-          <td><?php echo $row['data_nama']; ?></td>
-          <td>
-            <a href="edit.php?editdata&id=<?php echo $row['data_id']; ?>">Edit</a>
-            <a href="delete.php?deletedata&id=<?php echo $row['data_id']; ?>" style="float: right;" onclick="return confirm('Are you sure you want to delete name <?php echo $row['data_nama']; ?> ?')">Delete</a>
-          </td>
-        </tr>
-      </tbody>
-      <?php $no++ ?>
-      <?php } $conn->close(); ?>
-    </table>
-  </div>
+      <a href="add.php">Add</a> || <a href="https://ahmadabuhasan.me/github/API">API</a> || <a href="https://github.com/eby8zevin/ahmadabuhasan.me">Source Code</a>
+      <table border="1" width="700px">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>NIM</th>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php $no = 1; ?>
+          <?php $query = $conn->query("SELECT * FROM data_mhs ORDER BY data_nim ASC"); ?>
+          <?php while($row = $query->fetch_assoc()) { ?>
+          <tr>
+            <td style="text-align: center;"><?php echo $no; ?></td>
+            <td><?php echo $row['data_nim']; ?></td>
+            <td><?php echo $row['data_nama']; ?></td>
+            <td>
+              <a href="edit.php?editdata&id=<?php echo $row['data_id']; ?>">Edit</a>
+              <a href="delete.php?deletedata&id=<?php echo $row['data_id']; ?>" style="float: right;" onclick="return confirm('Are you sure you want to delete name <?php echo $row['data_nama']; ?> ?')">Delete</a>
+            </td>
+          </tr>
+        </tbody>
+        <?php $no++ ?>
+        <?php } $conn->close(); ?>
+      </table>
+    </div>
 
   </body>
 </html>
